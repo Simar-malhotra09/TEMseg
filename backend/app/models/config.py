453f3945e36561f1e52f096c1e14da76136dc0bc
Simar Malhotra <malhotrasimar009@gmail.com>
2026-02-16 12:00:50 -0500

    use to create pipelines based on components, each of which is an idv model

---
+++ backend/app/models/config.py
@@ -0,0 +1,12 @@
+from base_model import SubModelConfig, ModelConfig
+from settings import settings
+
+
+yolo_config = SubModelConfig("yolo", settings.YOLO_MODEL_PATH)
+sam_config = SubModelConfig("sam", settings.SAM_MODEL_PATH)
+
+nano_config = ModelConfig(
+    name="nano_pipeline",
+    components=[yolo_config, sam_config]
+)
+
