6e234a2bf00c85f35c69a03c73da40e730d1698f
Simar Malhotra <malhotrasimar009@gmail.com>
2026-02-16 10:21:37 -0500

    define shared seg res object

--- backend/app/models/base_model.py
+++ backend/app/models/base_model.py
@@ -1,5 +1,17 @@
 from abc import ABC, abstractmethod
 import cv2 as cv
+import numpy as np
+
+'''
+A simple way to structure this would be defining x,y,width and height.
+But our objects are not uniform in any sense.
+What we could do is, return a numpy arr of same h,w as img with each
+pixel representing an object id.
+'''
+@dataclass
+class SegmentationResult:
+    segmentation_masks: np.ndarray
+
 
 
 
