8b0d387910ed0fd71026be5bddf0c29c1c9022ed
Simar Malhotra <malhotrasimar009@gmail.com>
2026-02-16 10:43:01 -0500

    allow multiple models to be loaded as one

--- backend/app/models/base_model.py
+++ backend/app/models/base_model.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 import cv2 as cv
 import numpy as np
-
+from dataclasses import dataclass
+from typing import List
 '''
 A simple way to structure this would be defining x,y,width and height.
 But our objects are not uniform in any sense.
@@ -12,13 +13,20 @@ pixel representing an object id.
 class SegmentationResult:
     segmentation_masks: np.ndarray
 
+@dataclass
+class SubModelConfig:
+    name: str
+    path: str
 
+@dataclass
+class ModelConfig:
+    name: str
+    components: List[SubModelConfig]
 
 
 class Model(ABC):
-    def __init__(self, model_name, model_path):
-        self.model_name= model_name
-        self.model_path = model_path 
+    def __init__(self, config: ModelConfig):
+         self.config = config
 
     # model name, params, or any other helpful info
     @abstractmethod
