5f49c908bfa783523129d0aeb8aa75f301573258
Simar Malhotra <malhotrasimar009@gmail.com>
2026-08-14 08:44:47 -0400

    feat(models): add synthetic house model config

--- backend/src/app/models/helpers/config.py
+++ backend/src/app/models/helpers/config.py
@@ -4,15 +4,15 @@ from app.models.helpers.settings import settings
 
 yolo_config = SubModelConfig("yolo", settings.YOLO_MODEL_PATH)
 sam_config = SubModelConfig("sam", settings.SAM_MODEL_PATH)
-maskrcnn_config= SubModelConfig("maskrcnn", settings.MASKRCNN_MODEL_PATH)
-
-nano_config = ModelConfig(
-    name="nano_pipeline",
-    components=[yolo_config, sam_config]
+maskrcnn_config = SubModelConfig("maskrcnn", settings.MASKRCNN_MODEL_PATH)
+maskrcnn_synthetic_config = SubModelConfig(
+    "maskrcnn", settings.MASKRCNN_SYNTHETIC_MODEL_PATH
 )
 
-house_config= ModelConfig(
-        name="house_pipeline",
-        components=[maskrcnn_config]
-        )
+nano_config = ModelConfig(name="nano_pipeline", components=[yolo_config, sam_config])
 
+house_config = ModelConfig(name="house_pipeline", components=[maskrcnn_config])
+
+house_synthetic_config = ModelConfig(
+    name="house_pipeline_synthetic", components=[maskrcnn_synthetic_config]
+)
