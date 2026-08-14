894bdf9d361100dee71c8e39baf9a33521552055
Simar Malhotra <malhotrasimar009@gmail.com>
2026-08-14 08:44:52 -0400

    chore(models): update dev test script for MaskRCNN model_id param

--- backend/src/app/models/main.py
+++ backend/src/app/models/main.py
@@ -3,6 +3,7 @@ from app.models.base_model import SubModelConfig, ModelConfig, StatType, StatsCo
 from app.models.impls.yolosam import YoloSam
 from app.models.impls.maskrcnn import MaskRCNN
 from app.models.helpers.config import nano_config, house_config
+from app.api.live_models import AvailableModels
 from app.models.helpers.compute_stats import (
     compute_particle_count,
     compute_avg_size,
@@ -11,7 +12,7 @@ from app.models.helpers.compute_stats import (
 )
 import torch
 
-BASE_DIR = Path(__file__).resolve().parents[2]  
+BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = BASE_DIR / "data"
 
 
@@ -23,6 +24,8 @@ stats_config = StatsConfig(
         StatType.COVERAGE,
     }
 )
+
+
 def compute_stats(mask, config: StatsConfig):
     results = {}
 
@@ -41,18 +44,18 @@ def compute_stats(mask, config: StatsConfig):
     return results
 
 
-model1 = MaskRCNN(house_config, device="cpu")
+model1 = MaskRCNN(house_config, AvailableModels.maskrcnn, device="cpu")
 print(torch.cuda.is_available())
 model1.get_model_specs()
 
-img1= model1.load_image(DATA_DIR / "0032.tif")
+img1 = model1.load_image(DATA_DIR / "0032.tif")
 result1 = model1.segment(img1)
 
 model2 = YoloSam(nano_config, device="cpu")
 print(torch.cuda.is_available())
 model2.get_model_specs()
 
-img2= model2.load_image(DATA_DIR / "0032.tif")
+img2 = model2.load_image(DATA_DIR / "0032.tif")
 result2 = model2.segment(img2)
 if isinstance(result2.segmentation_mask, torch.Tensor):
     result2.segmentation_mask = result2.segmentation_mask.detach().cpu().numpy()
@@ -65,4 +68,3 @@ stats2 = compute_stats(result2.segmentation_mask, stats_config)
 
 print("MaskRCNN stats:", stats1)
 print("YoloSam stats:", stats2)
-
